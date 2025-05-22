0be830129a4fb24ace7051e73f329af7852ead55
miseullang <miseullang@gmail.com>
2025-05-22 15:48:27 +0900

    :sparkle:Feature: 컨텐츠 tiptap 뷰어로 불러오도록 구현

--- src/pages/NoticeDetail/components/NoticeDetailContent.tsx
+++ src/pages/NoticeDetail/components/NoticeDetailContent.tsx
@@ -4,31 +4,68 @@ import { ILocalization } from '@carebell/bell-core';
 import { Box, Typography } from '@mui/material';
 import { NoticeContentProps } from '@/types/NoticeDetail/NoticeContent.type';
 import NoticeDetailContentSkeleton from './NoticeDetailContentSkeleton';
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import Image from '@tiptap/extension-image';
 
 const NoticeDetailContent = ({ uuid }: NoticeContentProps) => {
   const [localization, setLocalization] = useState<ILocalization | null>(null);
   const [loading, setLoading] = useState(true);
 
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Image.configure({
+        HTMLAttributes: {
+          class: 'responsive max-content',
+        },
+      }),
+    ],
+    editable: false,
+  });
+
   useEffect(() => {
     setLoading(true);
     fetchLocalization(uuid)
-      .then(setLocalization)
+      .then((data) => {
+        setLocalization(data);
+        if (editor && data.default) {
+          editor.commands.setContent(data.default);
+        }
+      })
       .finally(() => setLoading(false));
-  }, [uuid]);
+  }, [uuid, editor]);
 
   if (loading) {
     return <NoticeDetailContentSkeleton />;
   }
 
-  if (!localization) return <Typography>데이터를 불러올 수 없습니다.</Typography>;
+  if (!localization)
+    return <Typography>데이터를 불러올 수 없습니다.</Typography>;
 
   return (
-    <Box pt={2}>
-      <Typography
-        variant='body1'
-        sx={{ color: 'var(--darkGray)' }}>
-        {localization.default}
-      </Typography>
+    <Box
+      pt={2}
+      sx={{
+        '.ProseMirror': {
+          '& img': {
+            maxWidth: '100%',
+            height: 'auto',
+            display: 'block',
+            margin: '0 auto',
+          },
+          '& p': {
+            margin: '1rem 0',
+            '&:first-of-type': {
+              marginTop: 0,
+            },
+            '&:last-of-type': {
+              marginBottom: 0,
+            },
+          },
+        },
+      }}>
+      <EditorContent editor={editor} />
     </Box>
   );
 };
